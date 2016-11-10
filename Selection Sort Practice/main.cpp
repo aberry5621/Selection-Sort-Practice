aa61e0349a684f6789770c952df337ddffe675fb
aberry5621 <aberry5621@gmail.com>
2016-11-09 16:12:45 -0800

    Initial Commit

---
+++ Selection Sort Practice/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Selection Sort Practice
+//
+//  Created by ax on 11/9/16.
+//  Copyright © 2016 COMP130. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
